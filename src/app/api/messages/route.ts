e79885b0a10cecac9562ec28682bf3150b2106e5
Muhammad Umar <umr.m2305@gmail.com>
2026-01-05 06:45:45 +0500

    minor fixes

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -4,13 +4,10 @@ import { redis } from "@/lib/redis";
 import { NextRequest, NextResponse } from "next/server";
 import { nanoid } from "nanoid";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
-  const { connectedUsers, roomID, token } = await auth(req);
+export async function POST(req: NextRequest) {
+  const { roomID, token } = await auth(req);
   const { sender, text }: { sender: string; text: string } = await req.json();
-  const roomExists = await redis.exists(`meta:${roomID}`);
-  if (!roomExists) {
-    throw new Error("Room does not exist");
-  }
+
   const message: Message = {
     id: nanoid(),
     sender,
@@ -19,32 +16,22 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
     timeStamp: Date.now(),
   };
 
-  await redis.rpush(`messages:${roomID}`, {
-    ...message,
-    token,
-  });
-
+  await redis.rpush(`messages:${roomID}`, { ...message, token });
   await realtime.channel(roomID).emit("chat.message", message);
-  const remainingTime = await redis.ttl(`meta:${roomID}`);
-  await redis.expire(`messages:${roomID}`, remainingTime);
-  await redis.expire(`history:${roomID}`, remainingTime);
-  await redis.expire(roomID, remainingTime);
 
-  return NextResponse.json({
-    success: true,
-    message: "message sent successfully",
-  });
-};
+  return NextResponse.json({ success: true });
+}
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
-  const { connectedUsers, roomID, token } = await auth(req);
+export async function GET(req: NextRequest) {
+  const { roomID, token } = await auth(req);
+
   const messages = await redis.lrange<Message>(`messages:${roomID}`, 0, -1);
+
   return NextResponse.json({
     success: true,
-    message: "messages received successfully successfully",
-    messages: messages.map((message) => ({
-      ...message,
-      token: message.token === token ? token : undefined,
+    messages: messages.map((m) => ({
+      ...m,
+      token: m.token === token ? token : undefined,
     })),
   });
-};
+}
